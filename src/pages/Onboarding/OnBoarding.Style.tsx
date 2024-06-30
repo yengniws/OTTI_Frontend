71b291215f9ff25516ce6633ae8b1da16c349056
김주하 <101048129+laketree2@users.noreply.github.com>
2024-07-01 03:13:44 +0900

    Modify: 로고 재수정

--- src/pages/Onboarding/OnBoarding.Style.tsx
+++ src/pages/Onboarding/OnBoarding.Style.tsx
@@ -10,20 +10,20 @@ export const OnboardingContainer = styled.div`
   max-width: 375px; // 최대 너비 설정
   height: 100vh;
   margin: 0 auto;
-  background-color: #333;
+  background-color: #222;
   color: #fff;
   box-sizing: border-box;
 `;
 
 // 로고 이미지
 export const Logo = styled.img`
-  width: 72px;
-  margin-bottom: 20px;
+  width: 60px;
+  margin-bottom: 14px;
 `;
 
 // 타이틀
 export const Title = styled.h1`
-  font-size: 2rem; // 크기를 줄임
+  font-size: 2.2rem; // 크기를 줄임
   font-weight: 300; // 폰트 굵기를 줄임
   margin: 0;
   letter-spacing: 1px; // 글자 간격을 약간 늘림
