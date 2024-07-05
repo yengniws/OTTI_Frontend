25217ba2798ddecb3d3ce5543a195e8483c62066
김주하 <101048129+laketree2@users.noreply.github.com>
2024-07-05 22:06:22 +0900

    Modify: piechart  style 수정

--- src/components/totalfee/PieChart.Style.tsx
+++ src/components/totalfee/PieChart.Style.tsx
@@ -5,40 +5,4 @@ export const ChartContainer = styled.div`
   padding-top: 20px;
   width: 100%;
   height: 300px;
-
-  canvas {
-    max-height: 100%;
-    max-width: 100%;
-  }
-
-  .chartjs-legend {
-    display: flex !important;
-    flex-direction: column !important;
-    align-items: flex-start !important;
-    position: absolute !important;
-    bottom: 0 !important;
-    left: 0 !important;
-    width: 100% !important;
-    padding: 20px 0 !important;
-  }
-
-  .chartjs-legend ul {
-    display: flex !important;
-    flex-direction: column !important;
-    align-items: flex-start !important;
-    width: 100% !important;
-    padding: 0 !important;
-    margin: 0 !important;
-  }
-
-  .chartjs-legend li {
-    margin-bottom: 10px !important;
-    display: flex !important;
-    align-items: center !important;
-    width: 100% !important;
-  }
-
-  .chartjs-legend li span {
-    margin-right: 10px !important;
-  }
 `;
