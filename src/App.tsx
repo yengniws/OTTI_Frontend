1aa660060d8fd16f908d973258c798333be00629
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-07-08 23:30:19 +0900

    Feat : 라우터 이동 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
+import AddOttSubscription from './pages/AddOttSubscription/AddOttSubscription';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/main" element={<Main />} />
+      <Route path="/main/addOttSubscription" element={<AddOttSubscription />} />
     </Routes>
   );
 };
