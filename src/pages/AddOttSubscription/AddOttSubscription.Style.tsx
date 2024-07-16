6c444c37df557aa4681c75f314f5c2ce2723b819
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-07-16 15:16:06 +0900

    Feat : ui 및 이미지 경로 수정

--- src/pages/AddOttSubscription/AddOttSubscription.Style.tsx
+++ src/pages/AddOttSubscription/AddOttSubscription.Style.tsx
@@ -1,77 +1,207 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  width: 90%;
+  max-width: 375px;
+  box-sizing: border-box;
+  padding: 20px;
+  background-color: #f4f4f4;
+  border-radius: 30px;
   display: flex;
   flex-direction: column;
-  width: 100%;
-  max-width: 375px;
-  height: 100vh;
-  box-sizing: border-box;
-  margin-top: 10px;
+  align-items: center;
 `;
 
-export const Form = styled.div`
-  background: white;
-  padding: 2rem;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  width: 300px;
-`;
-
-export const OTTSelector = styled.div`
+export const Header = styled.div`
   display: flex;
   align-items: center;
-  margin-bottom: 1rem;
+  margin: 20px;
+  width: 85%;
+  justify-content: space-between;
+`;
 
-  img {
-    width: 50px;
-    margin-right: 1rem;
-  }
+export const Image = styled.img`
+  width: 85px;
+  height: 85px;
+  border-radius: 50%;
+`;
 
-  select {
-    flex: 1;
+export const OttName = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+  margin: 0 15%;
+`;
+
+export const Text = styled.p`
+  font-size: 14px;
+  font-weight: bold;
+  margin: 0 10px;
+  flex: 1;
+  text-align: right;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+export const TextMemo = styled.p`
+  font-size: 14px;
+  margin: 0 10px;
+  flex: 1;
+  text-align: left;
+`;
+
+export const Label = styled.label`
+  font-size: 15px;
+  font-weight: bold;
+  margin: 10px 0;
+  display: block;
+  width: 50%;
+  text-align: left;
+`;
+
+export const LabelMemo = styled.label`
+  font-size: 15px;
+  font-weight: bold;
+  margin: 10px 0;
+  display: block;
+  width: 85%;
+  text-align: left;
+`;
+
+export const Input = styled.input`
+  width: 80%;
+  padding: 10px;
+  margin-bottom: 10px;
+  border: none;
+  font-size: 14px;
+  box-sizing: border-box;
+  background-color: inherit;
+  text-align: right;
+  font-weight: bold;
+
+  margin: 0 10px;
+  flex: 1;
+
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  &:focus {
+    outline: none;
   }
 `;
 
-export const InputField = styled.div`
-  margin-bottom: 1rem;
+export const InputMemo = styled.input`
+  width: 85%;
+  padding: 5px;
+  margin-bottom: 10px;
+  border: none;
+  font-size: 14px;
+  box-sizing: border-box;
+  background-color: inherit;
+  text-align: left;
 
-  label {
-    display: block;
-    margin-bottom: 0.5rem;
-  }
-
-  input,
-  select,
-  textarea {
-    width: 100%;
-    padding: 0.5rem;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-  }
-
-  textarea {
-    resize: none;
-    height: 100px;
+  &:focus {
+    outline: none;
   }
 `;
 
-export const SubmitButton = styled.button`
+export const SelectOttName = styled.select`
+  width: auto;
+  height: 40px;
+  padding: 10px;
+  border: none;
+  box-sizing: border-box;
+  background-color: inherit;
+  text-align: right;
+  font-weight: bold;
+  font-size: 20px;
+  margin: 0 15%;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const Select = styled.select`
   width: 100%;
-  padding: 0.75rem;
-  background-color: #000;
-  color: white;
+  height: auto;
+  padding: 10px;
+  border: none;
+  font-size: 14px;
+  box-sizing: border-box;
+  background-color: inherit;
+  text-align: right;
+  font-weight: bold;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const Button = styled.button`
+  width: 40%;
+  padding: 10px;
+  margin: 10px 5px;
   border: none;
   border-radius: 5px;
+  font-size: 14px;
   cursor: pointer;
-
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:nth-child(1) {
+    background-color: #333;
+    color: #fff;
+    opacity: 0.9;
+  }
+  &:nth-child(2) {
+    background-color: #f44336;
+    color: #fff;
+    opacity: 0.9;
+  }
   &:hover {
-    background-color: #444;
+    opacity: 1;
   }
 `;
 
-export const Message = styled.div`
-  margin-top: 1rem;
-  text-align: center;
-  color: green;
+export const ButtonSave = styled(Button)`
+  background-color: #333;
+  color: #fff;
+  opacity: 0.9;
+  width: 90%;
+  margin-left: 5%;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 85%;
+`;
+
+export const Section = styled.div`
+  display: flex;
+  align-items: center;
+  width: 85%;
+`;
+
+export const Divider = styled.hr`
+  width: 85%;
+  border: 0;
+  border-top: 0.5px solid #000;
+  margin: 10px 0;
+`;
+
+export const MemoBox = styled.div`
+  width: 85%;
+  height: 109px;
+  background-color: #fff;
+  padding: 15px;
+  margin-bottom: 10px;
+  box-sizing: border-box;
+  border-radius: 16px;
+  border: none;
 `;
--- src/pages/AddOttSubscription/AddOttSubscription.tsx
+++ src/pages/AddOttSubscription/AddOttSubscription.tsx
@@ -1,5 +1,3 @@
-//npm install react-toastify
-
 import React, { useState } from 'react';
 import axios from 'axios';
 import * as S from './AddOttSubscription.Style';
@@ -7,36 +5,42 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const ottOptions = [
-  //추후 이미지 경로 수정
+  //하드코딩 값, 이미지 경로는 소정이가 주는 호스팅 경로로 바꾸기
   {
     name: '넷플릭스',
-    img: '넷플릭스.png',
-    plans: ['광고형', '스탠다드', '프리미엄'],
+    imgURL:
+      'https://play-lh.googleusercontent.com/TBRwjS_qfJCSj1m7zZB93FnpJM5fSpMA_wUlFDLxWAb45T9RmwBvQd5cWR5viJJOhkI',
+    plans: ['광고형', '스탠다드', '프리미엄', '기타'],
   },
   {
     name: '티빙',
-    img: '티빙.png',
-    plans: ['광고형', '베이직', '스탠다드', '프리미엄'],
+    imgURL:
+      'https://play-lh.googleusercontent.com/eG3T5rqaV3ifl3MKV0fMi4V161wdhghpmqOMq-KYXOCN7Qy98yZhSsIj1gUBQ6YQqRg',
+    plans: ['광고형', '베이직', '스탠다드', '프리미엄', '기타'],
   },
   {
     name: '웨이브',
-    img: '웨이브.png',
-    plans: ['베이직', '스탠다드', '프리미엄'],
+    imgURL:
+      'https://play-lh.googleusercontent.com/7cuI7bdCeZbmc9anRXqpmxZPH92t5NEEbhTnj5by6skhZK_dlUg9kx--gqtLf-8c2K12',
+    plans: ['베이직', '스탠다드', '프리미엄', '기타'],
   },
   {
     name: '디즈니+',
-    img: '디즈니+.png',
-    plans: ['스탠다드', '프리미엄'],
+    imgURL:
+      'https://play-lh.googleusercontent.com/5wVn4qP4EIQV4BU2nzx2oDGeyDFU3Id2zlchCDNpqEGzyZssojerAql5ybq5RS59Ve0y',
+    plans: ['스탠다드', '프리미엄', '기타'],
   },
   {
     name: '쿠팡플레이',
-    img: '쿠팡플레이.png',
-    plans: ['쿠팡와우회원(단일요금제)'],
+    imgURL:
+      'https://play-lh.googleusercontent.com/b0eSS0LrM52IqHtp1xOkaW--Js1xDLmE3cejCOY3iQNTMIMSUZ6wGhW0izQfpM9m3bNu=w240-h480-rw',
+    plans: ['쿠팡와우회원', '기타'],
   },
   {
     name: '왓챠',
-    img: '왓챠.png',
-    plans: ['베이직', '프리미엄'],
+    imgURL:
+      'https://play-lh.googleusercontent.com/vAkKvTtE8kdb0MWWxOVaqYVf0_suB-WMnfCR1MslBsGjhI49dAfF1IxcnhtpL3PnjVY',
+    plans: ['베이직', '프리미엄', '기타'],
   },
 ];
 
@@ -60,7 +64,6 @@ const AddOttSubscription: React.FC = () => {
   const handleSubmit = async () => {
     try {
       await axios.post('http://localhost:3000/submissions', {
-        //추후 백엔드 경로 수정 필요
         ott: ott.name,
         name,
         plan,
@@ -84,61 +87,58 @@ const AddOttSubscription: React.FC = () => {
 
   return (
     <S.Container>
-      <S.Form>
-        <S.OTTSelector>
-          <img src={ott.img} alt={ott.name} />
-          <select
-            value={ott.name}
-            onChange={(e) => handleOttChange(e.target.value)}
-          >
-            {ottOptions.map((option) => (
-              <option key={option.name} value={option.name}>
-                {option.name}
-              </option>
-            ))}
-          </select>
-        </S.OTTSelector>
-        <S.InputField>
-          <label>이름</label>
-          <input value={name} onChange={(e) => setName(e.target.value)} />
-        </S.InputField>
-        <S.InputField>
-          <label>요금제 선택</label>
-          <select value={plan} onChange={(e) => setPlan(e.target.value)}>
-            {ott.plans.map((plan) => (
-              <option key={plan} value={plan}>
-                {plan}
-              </option>
-            ))}
-          </select>
-        </S.InputField>
-        <S.InputField>
-          <label>구독료</label>
-          <input
-            type="number"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-          />
-        </S.InputField>
-        <S.InputField>
-          <label>결제일</label>
-          <select value={date} onChange={(e) => setDate(e.target.value)}>
-            {dateOptions.map((day) => (
-              <option key={day} value={day.toString()}>
-                {`매월 ${day}일`}
-              </option>
-            ))}
-          </select>
-        </S.InputField>
-        <S.InputField>
-          <label>메모</label>
-          <textarea value={memo} onChange={(e) => setMemo(e.target.value)} />
-        </S.InputField>
-        <S.SubmitButton type="button" onClick={handleSubmit}>
-          등록하기
-        </S.SubmitButton>
-        {message && <S.Message>{message}</S.Message>}
-      </S.Form>
+      <S.Header>
+        <S.Image src={ott.imgURL} alt={ott.name} />
+        <S.SelectOttName
+          value={ott.name}
+          onChange={(e) => handleOttChange(e.target.value)}
+        >
+          {ottOptions.map((option) => (
+            <option key={option.name} value={option.name}>
+              {option.name}
+            </option>
+          ))}
+        </S.SelectOttName>
+      </S.Header>
+      <S.Section>
+        <S.Label>이름</S.Label>
+        <S.Input value={name} onChange={(e) => setName(e.target.value)} />
+      </S.Section>
+      <S.Divider />
+      <S.Section>
+        <S.Label>요금제 선택</S.Label>
+        <S.Select value={plan} onChange={(e) => setPlan(e.target.value)}>
+          {ott.plans.map((plan) => (
+            <option key={plan} value={plan}>
+              {plan}
+            </option>
+          ))}
+        </S.Select>
+      </S.Section>
+      <S.Divider />
+      <S.Section>
+        <S.Label>구독료</S.Label>
+        <S.Input value={amount} onChange={(e) => setAmount(e.target.value)} />
+      </S.Section>
+      <S.Divider />
+      <S.Section>
+        <S.Label>정기결제일</S.Label>
+        <S.Select value={date} onChange={(e) => setDate(e.target.value)}>
+          {dateOptions.map((day) => (
+            <option key={day} value={day.toString()}>
+              {`${day}일`}
+            </option>
+          ))}
+        </S.Select>
+      </S.Section>
+      <S.Divider />
+      <S.LabelMemo>메모</S.LabelMemo>
+      <S.MemoBox>
+        <S.InputMemo value={memo} onChange={(e) => setMemo(e.target.value)} />
+      </S.MemoBox>
+      <S.ButtonContainer>
+        <S.ButtonSave onClick={handleSubmit}>등록하기</S.ButtonSave>
+      </S.ButtonContainer>
     </S.Container>
   );
 };
