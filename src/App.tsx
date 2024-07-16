75605d71e88669c890398a16e189ed933dccc3bf
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-07-16 15:24:42 +0900

    Feat : 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
+import MyPage from './pages/MyPage/MyPage';
+import EditMyPage from './pages/MyPage/EditMyPage';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/main" element={<Main />} />
+      <Route path="/myPage" element={<MyPage />} />
+      <Route path="/editMyPage" element={<EditMyPage />} />
     </Routes>
   );
 };
---
+++ src/pages/MyPage/EditMyPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import * as S from './EditMyPage.Style';
+
+const Main = () => {
+  return (
+    <S.MainContainer>
+      <S.Header>
+        <div>OTTi</div>
+      </S.Header>
+    </S.MainContainer>
+  );
+};
+
+export default Main;
