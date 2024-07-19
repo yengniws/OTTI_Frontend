63d7f68f54febfeffdfbc661d22a033f3e3db090
김주하 <101048129+laketree2@users.noreply.github.com>
2024-07-19 21:27:26 +0900

    Fix: 오탈자 수정

--- src/api/test.js
+++ src/api/test.js
@@ -6,6 +6,12 @@ export const getSubscriptionById = async (subscriptionId) => {
   return response.data;
 };
 
+// 구독 추가: post /api/subscription
+export const addSubscription = async (data) => {
+  const response = await axios.post('/api/subscription', data);
+  return response.data;
+};
+
 // 구독 수정: put /api/subscription/subscription{id}
 export const updateSubscription = async (id, data) => {
   const response = await axios.put(`/api/subscription/subscription${id}`, data);
@@ -19,7 +25,6 @@ export const getAllSubscriptions = async (userId) => {
 };
 
 // 총 구독료: ?
-// API 경로가 명확하지 않아 주석 처리된 상태로 유지합니다.
 // export const getTotalSubscription = async () => {
 //   const response = await axios.get('/api/totalSubscription');
 //   return response.data;
