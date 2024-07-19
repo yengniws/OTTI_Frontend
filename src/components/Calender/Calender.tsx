853f8efd23f6a140aedf468d43aff96fd76a1ed8
김주하 <101048129+laketree2@users.noreply.github.com>
2024-07-19 23:41:28 +0900

    Fix: 반복 횟수 수정

--- src/components/Calender/Calender.tsx
+++ src/components/Calender/Calender.tsx
@@ -92,12 +92,255 @@
 
 // export default Calendar;
 
+// import React, { useState } from 'react';
+// import {
+//   Calendar as BigCalendar,
+//   momentLocalizer,
+//   Components,
+// } from 'react-big-calendar';
+// import moment from 'moment-timezone';
+// import 'moment/locale/ko';
+// import 'react-big-calendar/lib/css/react-big-calendar.css';
+// import * as S from './Calender.Style';
+// import Popup from './Popup';
+
+// moment.tz.setDefault('Asia/Seoul');
+// moment.locale('ko');
+// const localizer = momentLocalizer(moment);
+
+// interface IEvent {
+//   start: Date;
+//   end: Date;
+//   title: string;
+//   color: string;
+// }
+
+// const Calendar = () => {
+//   const [events] = useState<IEvent[]>([
+//     {
+//       start: new Date(2024, 7, 2),
+//       end: new Date(2024, 7, 2),
+//       title: 'Netflix',
+//       color: '#E50914',
+//     },
+//     {
+//       start: new Date(2024, 7, 15),
+//       end: new Date(2024, 7, 15),
+//       title: 'Disney+',
+//       color: '#0063e5',
+//     },
+//     {
+//       start: new Date(2024, 7, 20),
+//       end: new Date(2024, 7, 20),
+//       title: 'Wavve',
+//       color: '#00A8E1',
+//     },
+//   ]);
+//   const [currentMonth, setCurrentMonth] = useState(moment().format('M월'));
+//   const [selectedEvent, setSelectedEvent] = useState<IEvent | null>(null);
+
+//   const DateCellWrapper: Components['dateCellWrapper'] = ({
+//     children,
+//     value,
+//   }) => {
+//     const event = events.find((evt) => moment(evt.start).isSame(value, 'day'));
+
+//     return React.cloneElement(
+//       React.Children.only(children) as React.ReactElement,
+//       {
+//         style: {
+//           ...((children as React.ReactElement).props.style || {}),
+//           position: 'relative',
+//           cursor: event ? 'pointer' : 'default',
+//           ...(event && {
+//             '&::after': {
+//               content: '""',
+//               position: 'absolute',
+//               bottom: '2px',
+//               left: '50%',
+//               transform: 'translateX(-50%)',
+//               width: '80%',
+//               height: '2px',
+//               backgroundColor: event.color,
+//             },
+//           }),
+//         },
+//       },
+//     );
+//   };
+
+//   const handleNavigate = (newDate: Date) => {
+//     setCurrentMonth(moment(newDate).format('M월'));
+//   };
+
+//   const handleSelectEvent = (event: IEvent) => {
+//     setSelectedEvent(event);
+//   };
+
+//   return (
+//     <S.CalendarCont>
+//       <S.MonthTit>{currentMonth}</S.MonthTit>
+//       <S.CalendarWrap>
+//         <BigCalendar
+//           localizer={localizer}
+//           events={events}
+//           startAccessor="start"
+//           endAccessor="end"
+//           style={{ height: 210 }}
+//           views={['month']}
+//           toolbar={false}
+//           onNavigate={handleNavigate}
+//           onSelectEvent={handleSelectEvent}
+//           components={{
+//             dateCellWrapper: DateCellWrapper,
+//           }}
+//           formats={{
+//             monthHeaderFormat: 'M월',
+//             weekdayFormat: (date: Date) => moment(date).format('ddd'),
+//             dayFormat: (date: Date) => moment(date).format('D'),
+//           }}
+//         />
+//       </S.CalendarWrap>
+//       {selectedEvent && (
+//         <Popup event={selectedEvent} onClose={() => setSelectedEvent(null)} />
+//       )}
+//     </S.CalendarCont>
+//   );
+// };
+
+// export default Calendar;
+
+// import React, { useState } from 'react';
+// import {
+//   Calendar as BigCalendar,
+//   momentLocalizer,
+//   Components,
+// } from 'react-big-calendar';
+// import { RRule } from 'rrule';
+// import moment from 'moment-timezone';
+// import 'moment/locale/ko';
+// import 'react-big-calendar/lib/css/react-big-calendar.css';
+// import * as S from './Calender.Style';
+// import Popup from './Popup';
+
+// moment.tz.setDefault('Asia/Seoul');
+// moment.locale('ko');
+// const localizer = momentLocalizer(moment);
+
+// interface IEvent {
+//   start: Date;
+//   end: Date;
+//   title: string;
+//   color: string;
+// }
+
+// const Calendar = () => {
+//   const [currentMonth, setCurrentMonth] = useState(moment().format('M월'));
+//   const [selectedEvent, setSelectedEvent] = useState<IEvent | null>(null);
+
+//   const createRecurringEvents = (
+//     baseDate: Date,
+//     title: string,
+//     color: string,
+//   ): IEvent[] => {
+//     const rule = new RRule({
+//       freq: RRule.MONTHLY,
+//       dtstart: baseDate,
+//       count: 12, //반복 횟수
+//     });
+
+//     return rule.all().map((date) => ({
+//       start: new Date(date),
+//       end: new Date(date),
+//       title,
+//       color,
+//     }));
+//   };
+
+//   const events = [
+//     ...createRecurringEvents(new Date(2024, 6, 2), 'Netflix', '#E50914'),
+//     ...createRecurringEvents(new Date(2024, 6, 15), 'Disney+', '#0063e5'),
+//     ...createRecurringEvents(new Date(2024, 6, 20), 'Wavve', '#00A8E1'),
+//   ];
+
+//   const DateCellWrapper: Components['dateCellWrapper'] = ({
+//     children,
+//     value,
+//   }) => {
+//     const event = events.find((evt) => moment(evt.start).isSame(value, 'day'));
+
+//     return React.cloneElement(
+//       React.Children.only(children) as React.ReactElement,
+//       {
+//         style: {
+//           ...((children as React.ReactElement).props.style || {}),
+//           position: 'relative',
+//           cursor: event ? 'pointer' : 'default',
+//           ...(event && {
+//             '&::after': {
+//               content: '""',
+//               position: 'absolute',
+//               bottom: '2px',
+//               left: '50%',
+//               transform: 'translateX(-50%)',
+//               width: '80%',
+//               height: '2px',
+//               backgroundColor: event.color,
+//             },
+//           }),
+//         },
+//       },
+//     );
+//   };
+
+//   const handleNavigate = (newDate: Date) => {
+//     setCurrentMonth(moment(newDate).format('M월'));
+//   };
+
+//   const handleSelectEvent = (event: IEvent) => {
+//     setSelectedEvent(event);
+//   };
+
+//   return (
+//     <S.CalendarCont>
+//       <S.MonthTit>{currentMonth}</S.MonthTit>
+//       <S.CalendarWrap>
+//         <BigCalendar
+//           localizer={localizer}
+//           events={events}
+//           startAccessor="start"
+//           endAccessor="end"
+//           style={{ height: 210 }}
+//           views={['month']}
+//           toolbar={false}
+//           onNavigate={handleNavigate}
+//           onSelectEvent={handleSelectEvent}
+//           components={{
+//             dateCellWrapper: DateCellWrapper,
+//           }}
+//           formats={{
+//             monthHeaderFormat: 'M월',
+//             weekdayFormat: (date: Date) => moment(date).format('ddd'),
+//             dayFormat: (date: Date) => moment(date).format('D'),
+//           }}
+//         />
+//       </S.CalendarWrap>
+//       {selectedEvent && (
+//         <Popup event={selectedEvent} onClose={() => setSelectedEvent(null)} />
+//       )}
+//     </S.CalendarCont>
+//   );
+// };
+
+// export default Calendar;
+
 import React, { useState } from 'react';
 import {
   Calendar as BigCalendar,
   momentLocalizer,
   Components,
 } from 'react-big-calendar';
+import { RRule } from 'rrule';
 import moment from 'moment-timezone';
 import 'moment/locale/ko';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
@@ -116,28 +359,33 @@ interface IEvent {
 }
 
 const Calendar = () => {
-  const [events] = useState<IEvent[]>([
-    {
-      start: new Date(2024, 7, 2),
-      end: new Date(2024, 7, 2),
-      title: 'Netflix',
-      color: '#E50914',
-    },
-    {
-      start: new Date(2024, 7, 15),
-      end: new Date(2024, 7, 15),
-      title: 'Disney+',
-      color: '#0063e5',
-    },
-    {
-      start: new Date(2024, 7, 20),
-      end: new Date(2024, 7, 20),
-      title: 'Wavve',
-      color: '#00A8E1',
-    },
-  ]);
   const [currentMonth, setCurrentMonth] = useState(moment().format('M월'));
   const [selectedEvent, setSelectedEvent] = useState<IEvent | null>(null);
+
+  const createRecurringEvents = (
+    baseDate: Date,
+    title: string,
+    color: string,
+  ): IEvent[] => {
+    const rule = new RRule({
+      freq: RRule.MONTHLY,
+      dtstart: baseDate,
+      interval: 1,
+    });
+
+    return rule.all().map((date) => ({
+      start: new Date(date),
+      end: new Date(date),
+      title,
+      color,
+    }));
+  };
+
+  const events = [
+    ...createRecurringEvents(new Date(2024, 6, 2), 'Netflix', '#E50914'),
+    ...createRecurringEvents(new Date(2024, 6, 15), 'Disney+', '#0063e5'),
+    ...createRecurringEvents(new Date(2024, 6, 20), 'Wavve', '#00A8E1'),
+  ];
 
   const DateCellWrapper: Components['dateCellWrapper'] = ({
     children,
