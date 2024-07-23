f5139065cd1e19e186bd35aef14180ae4ca002a7
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-07-23 18:04:22 +0900

    Feat : 엔드포인트 수정

--- src/pages/SubscriptionDetail/SubscriptionDetail.tsx
+++ src/pages/SubscriptionDetail/SubscriptionDetail.tsx
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import * as S from './SubscriptionDetail.Style';
-import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ottOptions, dateOptions } from '../../constants/constants';
+import axiosInstance from '../../libs/AxiosInstance';
 
 interface SubscriptionData {
-  ottName: string;
+  ott_name: string;
   customName: string;
-  plan: string;
-  paymentAmount: number;
-  paymentDate: number;
+  rate_plan: string;
+  price: number;
+  payment_date: number;
   memo: string;
-  imgURL: string;
+  ott_image: string;
 }
 
 const SubscriptionDetail: React.FC = () => {
@@ -24,58 +24,54 @@ const SubscriptionDetail: React.FC = () => {
   );
   const [editing, setEditing] = useState(false);
   const [formData, setFormData] = useState<SubscriptionData>({
-    ottName: '',
+    ott_name: '',
     customName: '',
-    plan: '',
-    paymentAmount: 0,
-    paymentDate: 1,
+    rate_plan: '',
+    price: 0,
+    payment_date: 1,
     memo: '',
-    imgURL: '',
+    ott_image: '',
   });
 
   useEffect(() => {
     const fetchSubscription = async () => {
       try {
-        const response = await axios.get(
-          `http://localhost:3000/subscriptions/${id}`,
+        const response = await axiosInstance.get(
+          `/api/subscription/{subscriptionid}`,
         );
         setSubscription(response.data);
         setFormData(response.data);
       } catch (error) {
-        console.error('구독 정보를 불러오는 중 오류 발생:', error);
+        console.error('데이터 가져오는 중 에러:', error);
       }
     };
     fetchSubscription();
   }, [id]);
 
   const handleDelete = () => {
-    axios
-      .delete(`http://localhost:3000/subscriptions/${id}`)
+    axiosInstance
+      .delete(`/api/subscription/{subscriptionid}`)
       .then(() => {
-        toast.success('삭제되었어요!');
+        toast.success('삭제 되었어요!');
         setTimeout(() => {
           navigate('/main');
         }, 2000);
       })
-      .catch((error) =>
-        console.error('구독 정보를 삭제하는 중 오류 발생:', error),
-      );
+      .catch((error) => console.error('데이터 삭제 중 에러:', error));
   };
 
   const handleSave = () => {
-    axios
-      .put(`http://localhost:3000/subscriptions/${id}`, {
+    axiosInstance
+      .put(`/api/subscription/{subscriptionid}`, {
         ...formData,
-        paymentDate: formData.paymentDate,
+        payment_date: formData.payment_date,
       })
       .then((response) => {
         setSubscription(response.data);
         setEditing(false);
-        toast.success('저장되었어요!');
+        toast.success('저장 되었어요!');
       })
-      .catch((error) =>
-        console.error('구독 정보를 업데이트하는 중 오류 발생:', error),
-      );
+      .catch((error) => console.error('데이터 업데이트 중 에러:', error));
   };
 
   const handleChange = (
@@ -84,20 +80,21 @@ const SubscriptionDetail: React.FC = () => {
     const { name, value } = e.target;
     setFormData((prevFormData) => ({
       ...prevFormData,
-      [name]: name === 'paymentDate' ? parseInt(value) : value,
+      [name]:
+        name === 'price' || name === 'payment_date' ? parseInt(value) : value,
     }));
 
-    if (name === 'ottName') {
+    if (name === 'ott_name') {
       handleOttNameChange(value);
     }
   };
 
-  const handleOttNameChange = (ottName: string) => {
-    const ott = ottOptions.find((option) => option.name === ottName);
+  const handleOttNameChange = (ott_name: string) => {
+    const ott = ottOptions.find((option) => option.name === ott_name);
     if (ott) {
       setFormData((prevFormData) => ({
         ...prevFormData,
-        imgURL: ott.imgURL,
+        ott_image: ott.imgURL,
       }));
     }
   };
@@ -107,11 +104,11 @@ const SubscriptionDetail: React.FC = () => {
       {subscription && (
         <>
           <S.Header>
-            <S.Image src={formData.imgURL} alt={formData.ottName} />
+            <S.Image src={formData.ott_image} alt={formData.ott_name} />
             {editing ? (
               <S.SelectOttName
-                name="ottName"
-                value={formData.ottName}
+                name="ott_name"
+                value={formData.ott_name}
                 onChange={handleChange}
               >
                 {ottOptions.map((option) => (
@@ -121,7 +118,7 @@ const SubscriptionDetail: React.FC = () => {
                 ))}
               </S.SelectOttName>
             ) : (
-              <S.OttName>{subscription.ottName}</S.OttName>
+              <S.OttName>{subscription.ott_name}</S.OttName>
             )}
           </S.Header>
 
@@ -143,12 +140,12 @@ const SubscriptionDetail: React.FC = () => {
             <S.Label>요금제</S.Label>
             {editing ? (
               <S.Select
-                name="plan"
-                value={formData.plan}
+                name="rate_plan"
+                value={formData.rate_plan}
                 onChange={handleChange}
               >
                 {ottOptions
-                  .find((option) => option.name === formData.ottName)
+                  .find((option) => option.name === formData.ott_name)
                   ?.plans.map((plan) => (
                     <option key={plan} value={plan}>
                       {plan}
@@ -156,7 +153,7 @@ const SubscriptionDetail: React.FC = () => {
                   ))}
               </S.Select>
             ) : (
-              <S.Text>{subscription.plan}</S.Text>
+              <S.Text>{subscription.rate_plan}</S.Text>
             )}
           </S.Section>
           <S.Divider />
@@ -165,12 +162,12 @@ const SubscriptionDetail: React.FC = () => {
             <S.Label>결제 금액</S.Label>
             {editing ? (
               <S.Input
-                name="paymentAmount"
-                value={formData.paymentAmount}
+                name="price"
+                value={formData.price}
                 onChange={handleChange}
               />
             ) : (
-              <S.Text>{subscription.paymentAmount}원</S.Text>
+              <S.Text>{subscription.price}원</S.Text>
             )}
           </S.Section>
           <S.Divider />
@@ -179,8 +176,8 @@ const SubscriptionDetail: React.FC = () => {
             <S.Label>정기결제일</S.Label>
             {editing ? (
               <S.Select
-                name="paymentDate"
-                value={formData.paymentDate.toString()}
+                name="payment_date"
+                value={formData.payment_date.toString()}
                 onChange={handleChange}
               >
                 {dateOptions.map((date) => (
@@ -190,7 +187,7 @@ const SubscriptionDetail: React.FC = () => {
                 ))}
               </S.Select>
             ) : (
-              <S.Text>{`매월 ${subscription.paymentDate}일`}</S.Text>
+              <S.Text>{`매월 ${subscription.payment_date}일`}</S.Text>
             )}
           </S.Section>
           <S.Divider />
