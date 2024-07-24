1007d5f63d3aa1eeffdf5355a603d1b9d57e3674
김주하 <101048129+laketree2@users.noreply.github.com>
2024-07-25 02:39:11 +0900

    Fix: api 활용 메서드만 남겨둠

--- src/api/subscriptionApi.ts
+++ src/api/subscriptionApi.ts
@@ -1,47 +1,10 @@
 import axios from 'axios';
 
-// 총 구독료
-export const getTotalAmount = async () => {
-  try {
-    const response = await axios.get(
-      '/http://localhost:3000/subscription/total',
-    );
-    return response.data.totalAmount;
-  } catch (error) {
-    console.error('N/A:', error);
-    throw error;
-  }
-};
-
 // 한 사람의 전체 구독 정보 조회
 // /api/subscription/user/{userid}
 export const getUserSubscription = async (userId: number) => {
   try {
     const response = await axios.get(`/api/subscription/user/${userId}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching subscription data:', error);
-    throw error;
-  }
-};
-
-// ott 추가
-// /api/subscription
-export const getSubscriptionData = async () => {
-  try {
-    const response = await axios.post('/api/subscription');
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching subscription data:', error);
-    throw error;
-  }
-};
-
-// 특정 ott 정보 조회
-// /api/subscription/{subscriptionid}
-export const getSubscriptionId = async () => {
-  try {
-    const response = await axios.post('/api/subscription/{subscriptionid}');
     return response.data;
   } catch (error) {
     console.error('Error fetching subscription data:', error);
