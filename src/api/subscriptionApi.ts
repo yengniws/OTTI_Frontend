aa70147d7f9da1bdf6c213f8db0bc7584b17c099
김주하 <101048129+laketree2@users.noreply.github.com>
2024-07-25 02:38:26 +0900

    Fix: api 코드로 변환

--- src/api/subscriptionApi.ts
+++ src/api/subscriptionApi.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+// 총 구독료
 export const getTotalAmount = async () => {
   try {
     const response = await axios.get(
@@ -12,19 +13,35 @@ export const getTotalAmount = async () => {
   }
 };
 
-export const getUserSubscriptions = async (userId: number) => {
+// 한 사람의 전체 구독 정보 조회
+// /api/subscription/user/{userid}
+export const getUserSubscription = async (userId: number) => {
   try {
     const response = await axios.get(`/api/subscription/user/${userId}`);
     return response.data;
   } catch (error) {
-    console.error('N/A:', error);
+    console.error('Error fetching subscription data:', error);
     throw error;
   }
 };
 
+// ott 추가
+// /api/subscription
 export const getSubscriptionData = async () => {
   try {
     const response = await axios.post('/api/subscription');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching subscription data:', error);
+    throw error;
+  }
+};
+
+// 특정 ott 정보 조회
+// /api/subscription/{subscriptionid}
+export const getSubscriptionId = async () => {
+  try {
+    const response = await axios.post('/api/subscription/{subscriptionid}');
     return response.data;
   } catch (error) {
     console.error('Error fetching subscription data:', error);
