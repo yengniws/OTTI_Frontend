6c2e1e90c836558278c81ab033003bdafbbfd7f3
김주하 <101048129+laketree2@users.noreply.github.com>
2024-07-26 23:38:18 +0900

    Feat: 알림 변경

--- src/components/NotificationIcon.Style.ts
+++ src/components/NotificationIcon.Style.ts
@@ -12,14 +12,14 @@ export const NotificationIconWrapper = styled.div`
 export const UnreadBadge = styled.span`
   position: absolute;
   top: 28px;
-  right: -2px;
+  right: -1px;
   background-color: red;
   color: white;
   border-radius: 50%;
   padding: 2px 2px;
   font-size: 11px;
-  min-width: 17px;
-  height: 17px;
+  min-width: 16px;
+  height: 16px;
   display: flex;
   align-items: center;
   justify-content: center;
