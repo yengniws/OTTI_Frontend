e4dea3a05ca509e6b427786a9baa2e21fd38af54
김주하 <101048129+laketree2@users.noreply.github.com>
2024-07-30 00:50:22 +0900

    Fix: topbar 크기 수정

--- src/components/topbar/TopBar.Style.tsx
+++ src/components/topbar/TopBar.Style.tsx
@@ -9,13 +9,14 @@ export const TopBarContainer = styled.div`
 export const BackButton = styled.button`
   background: none;
   border: none;
-  font-size: 24px;
+  font-size: 20px;
   cursor: pointer;
   padding-right: 8px;
 `;
 
 export const Title = styled.h1`
-  font-size: 18px;
+  font-size: 16px;
   font-weight: 400;
   margin: 0;
+  padding-bottom: 2px;
 `;
