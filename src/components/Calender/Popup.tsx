7456243eaa4986e6fe53ee88b9540a72cb5a914e
김주하 <101048129+laketree2@users.noreply.github.com>
2024-07-29 23:45:35 +0900

    Fix: 중간 세이브

--- src/components/Calender/Popup.tsx
+++ src/components/Calender/Popup.tsx
@@ -1,3 +1,37 @@
+// import React from 'react';
+// import * as S from './Popup.Style';
+// import moment from 'moment';
+
+// interface IEvent {
+//   start: Date;
+//   end: Date;
+//   title: string;
+//   color: string;
+// }
+
+// interface PopupProps {
+//   event: IEvent;
+//   onClose: () => void;
+// }
+
+// const Popup = ({ event, onClose }: PopupProps) => {
+//   return (
+//     <S.PopupCont>
+//       <S.PopupContent>
+//         <S.CloseBtn onClick={onClose}>X</S.CloseBtn>
+//         <S.EventDate>{moment(event.start).format('M/D')}</S.EventDate>
+//         <S.EventTitWrapper>
+//           <S.ColorDot style={{ backgroundColor: event.color }} />
+//           <S.EventTit>{event.title}</S.EventTit>
+//           <S.EventSubtitle>결제일</S.EventSubtitle>
+//         </S.EventTitWrapper>
+//       </S.PopupContent>
+//     </S.PopupCont>
+//   );
+// };
+
+// export default Popup;
+
 import React from 'react';
 import * as S from './Popup.Style';
 import moment from 'moment';
@@ -10,21 +44,26 @@ interface IEvent {
 }
 
 interface PopupProps {
-  event: IEvent;
+  day: Date;
+  events: IEvent[];
   onClose: () => void;
 }
 
-const Popup = ({ event, onClose }: PopupProps) => {
+const Popup = ({ day, events, onClose }: PopupProps) => {
   return (
     <S.PopupCont>
       <S.PopupContent>
         <S.CloseBtn onClick={onClose}>X</S.CloseBtn>
-        <S.EventDate>{moment(event.start).format('M/D')}</S.EventDate>
-        <S.EventTitWrapper>
-          <S.ColorDot style={{ backgroundColor: event.color }} />
-          <S.EventTit>{event.title}</S.EventTit>
-          <S.EventSubtitle>결제일</S.EventSubtitle>
-        </S.EventTitWrapper>
+        <S.EventDate>{moment(day).format('M/D')}</S.EventDate>
+        {events.map((event, index) => (
+          <div key={index}>
+            <S.EventTitWrapper>
+              <S.ColorDot style={{ backgroundColor: event.color }} />
+              <S.EventTit>{event.title}</S.EventTit>
+              <S.EventSubtitle>결제일</S.EventSubtitle>
+            </S.EventTitWrapper>
+          </div>
+        ))}
       </S.PopupContent>
     </S.PopupCont>
   );
