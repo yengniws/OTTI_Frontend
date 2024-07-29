2fb9cba4646e8714b350d2856b91b293cb9b864e
김주하 <101048129+laketree2@users.noreply.github.com>
2024-07-30 00:48:19 +0900

    Fix: type 문제 해결

--- src/pages/Main/SubscriptionDetail.tsx
+++ src/pages/Main/SubscriptionDetail.tsx
@@ -28,6 +28,41 @@
 
 // export default SubscriptionDetail;
 
+// import React from 'react';
+// import { useLocation } from 'react-router-dom';
+// import TopBar from '../../components/topbar/TopBar';
+// import TotalSubscriptionFee from '../../components/totalfee/TotalSubscriptionFee';
+// import PieChart from '../../components/totalfee/PieChart';
+// import * as S from './SubscriptionDetail.Style';
+// import * as T from './Main.Style';
+
+// interface LocationState {
+//   totalAmount: number;
+// }
+
+// const SubscriptionDetail: React.FC = () => {
+//   const location = useLocation();
+//   const state = location.state as LocationState | null;
+//   const totalAmount = state?.totalAmount ?? 0;
+
+//   return (
+//     <T.MainContainer>
+//       <S.TitleWrapper>
+//         <TopBar title="이번 달 총 구독료" />
+//       </S.TitleWrapper>
+//       <T.PageContainer>
+//         <TotalSubscriptionFee totalAmount={totalAmount} />
+//         <S.PieChartTitle>한 눈에 보기</S.PieChartTitle>
+//         <S.DetailContainer>
+//           <PieChart />
+//         </S.DetailContainer>
+//       </T.PageContainer>
+//     </T.MainContainer>
+//   );
+// };
+
+// export default SubscriptionDetail;
+
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import TopBar from '../../components/topbar/TopBar';
@@ -51,7 +86,7 @@ const SubscriptionDetail: React.FC = () => {
         <TopBar title="이번 달 총 구독료" />
       </S.TitleWrapper>
       <T.PageContainer>
-        <TotalSubscriptionFee totalAmount={totalAmount} />
+        <TotalSubscriptionFee initialTotalAmount={totalAmount} />
         <S.PieChartTitle>한 눈에 보기</S.PieChartTitle>
         <S.DetailContainer>
           <PieChart />
