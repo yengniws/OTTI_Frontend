57ea44bd81b96f20c4a83f7df0a75ec0c7bfaf4a
김주하 <101048129+laketree2@users.noreply.github.com>
2024-08-30 03:55:08 +0900

    Fix: 파이차트 열심히 삽질하기

--- src/components/totalfee/PieChart.tsx
+++ src/components/totalfee/PieChart.tsx
@@ -1,4 +1,102 @@
-// 깡코드
+// import React from 'react';
+// import { Pie } from 'react-chartjs-2';
+// import * as S from './PieChart.Style';
+// import {
+//   Chart as ChartJS,
+//   ArcElement,
+//   Tooltip,
+//   Legend,
+//   ChartData,
+//   ChartOptions,
+//   TooltipItem,
+// } from 'chart.js';
+
+// ChartJS.register(ArcElement, Tooltip, Legend);
+
+// const PieChart: React.FC = () => {
+//   const data: ChartData<'pie', number[], string> = {
+//     labels: ['Netflix', 'Disney+', 'Wavve'],
+//     datasets: [
+//       {
+//         data: [9500, 7900, 10900],
+//         backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+//       },
+//     ],
+//   };
+
+//   const options: ChartOptions<'pie'> = {
+//     responsive: true,
+//     maintainAspectRatio: false,
+//     plugins: {
+//       legend: {
+//         position: 'bottom',
+//         align: 'start',
+//         labels: {
+//           boxWidth: 12,
+//           padding: 20,
+//           font: {
+//             size: 12,
+//           },
+//           generateLabels: (chart) => {
+//             const datasets = chart.data.datasets[0];
+//             if (!datasets) {
+//               return [];
+//             }
+//             const total = datasets.data.reduce((sum, val) => {
+//               return sum + (typeof val === 'number' ? val : 0);
+//             }, 0);
+//             return (
+//               chart.data.labels?.map((label, i) => {
+//                 const value = datasets.data[i];
+//                 if (typeof value === 'number') {
+//                   const percentage = ((value / total) * 100).toFixed(2);
+//                   return {
+//                     text: `${label}: ${value}원 (${percentage}%)`,
+//                     fillStyle: Array.isArray(datasets.backgroundColor)
+//                       ? datasets.backgroundColor[i]
+//                       : '',
+//                     hidden: false,
+//                     index: i,
+//                   };
+//                 }
+//                 return {
+//                   text: `${label}: $0 (0%)`,
+//                   fillStyle: '',
+//                   hidden: true,
+//                   index: i,
+//                 };
+//               }) || []
+//             );
+//           },
+//         },
+//       },
+//       tooltip: {
+//         callbacks: {
+//           label: function (context: TooltipItem<'pie'>) {
+//             const label = context.label || '';
+//             const value = context.raw as number;
+//             return `${label}: $${value}`;
+//           },
+//         },
+//       },
+//     },
+//     layout: {
+//       padding: {
+//         top: 10,
+//         bottom: 10,
+//       },
+//     },
+//   };
+
+//   return (
+//     <S.ChartContainer>
+//       <Pie data={data} options={options} />
+//     </S.ChartContainer>
+//   );
+// };
+
+// export default PieChart;
+
 // import React, { useState, useEffect } from 'react';
 // import { Pie } from 'react-chartjs-2';
 // import * as S from './PieChart.Style';
@@ -20,7 +118,129 @@
 //   value: number;
 // }
 
-// // 동적으로 색상을 생성
+// const PieChart = () => {
+//   const [subscriptionData, setSubscriptionData] = useState<SubscriptionData[]>(
+//     [],
+//   );
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.post('/api/subscription');
+//         setSubscriptionData(response.data);
+//       } catch (error) {
+//         console.error('Error fetching subscription data:', error);
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   const data: ChartData<'pie', number[], string> = {
+//     labels: subscriptionData.map((item) => item.label),
+//     datasets: [
+//       {
+//         data: subscriptionData.map((item) => item.value),
+//         backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+//       },
+//     ],
+//   };
+
+//   const options: ChartOptions<'pie'> = {
+//     responsive: true,
+//     maintainAspectRatio: false,
+//     plugins: {
+//       legend: {
+//         position: 'bottom',
+//         align: 'start',
+//         labels: {
+//           boxWidth: 12,
+//           padding: 20,
+//           font: {
+//             size: 12,
+//           },
+//           generateLabels: (chart) => {
+//             const datasets = chart.data.datasets[0];
+//             if (!datasets) {
+//               return [];
+//             }
+//             const total = datasets.data.reduce((sum, val) => {
+//               return sum + (typeof val === 'number' ? val : 0);
+//             }, 0);
+//             return (
+//               chart.data.labels?.map((label, i) => {
+//                 const value = datasets.data[i];
+//                 if (typeof value === 'number') {
+//                   const percentage = ((value / total) * 100).toFixed(2);
+//                   return {
+//                     text: `${label}: ${value}원 (${percentage}%)`,
+//                     fillStyle: Array.isArray(datasets.backgroundColor)
+//                       ? datasets.backgroundColor[i]
+//                       : '',
+//                     hidden: false,
+//                     index: i,
+//                   };
+//                 }
+//                 return {
+//                   text: `${label}: 0원 (0%)`,
+//                   fillStyle: '',
+//                   hidden: true,
+//                   index: i,
+//                 };
+//               }) || []
+//             );
+//           },
+//         },
+//       },
+//       tooltip: {
+//         callbacks: {
+//           label: function (context: TooltipItem<'pie'>) {
+//             const label = context.label || '';
+//             const value = context.raw as number;
+//             return `${label}: ${value}원`;
+//           },
+//         },
+//       },
+//     },
+//     layout: {
+//       padding: {
+//         top: 10,
+//         bottom: 10,
+//       },
+//     },
+//   };
+
+//   return (
+//     <S.ChartContainer>
+//       <Pie data={data} options={options} />
+//     </S.ChartContainer>
+//   );
+// };
+
+// export default PieChart;
+
+// import React, { useState, useEffect } from 'react';
+// import { Pie } from 'react-chartjs-2';
+// import * as S from './PieChart.Style';
+// import axios from 'axios';
+// import {
+//   Chart as ChartJS,
+//   ArcElement,
+//   Tooltip,
+//   Legend,
+//   ChartData,
+//   ChartOptions,
+//   TooltipItem,
+// } from 'chart.js';
+
+// ChartJS.register(ArcElement, Tooltip, Legend);
+
+// interface SubscriptionData {
+//   label: string;
+//   value: number;
+// }
+
+// // 동적으로 색상을 생성하는 함수
 // const generateColors = (count: number): string[] => {
 //   const baseColors = [
 //     '#FF6384',
@@ -140,304 +360,10 @@
 
 // export default PieChart;
 
-// api 활용 - 1
-// import React, { useState, useEffect } from 'react';
-// import { Pie } from 'react-chartjs-2';
-// import * as S from './PieChart.Style';
-// import { getUserSubscription } from '../../api/subscriptionApi';
-// import {
-//   Chart as ChartJS,
-//   ArcElement,
-//   Tooltip,
-//   Legend,
-//   ChartData,
-//   ChartOptions,
-//   TooltipItem,
-// } from 'chart.js';
-
-// ChartJS.register(ArcElement, Tooltip, Legend);
-
-// interface SubscriptionData {
-//   label: string;
-//   value: number;
-// }
-
-// // 동적으로 색상을 생성하는 함수
-// const generateColors = (count: number): string[] => {
-//   const baseColors = [
-//     '#FF6384',
-//     '#36A2EB',
-//     '#FFCE56',
-//     '#4BC0C0',
-//     '#9966FF',
-//     '#FF9F40',
-//   ];
-//   const colors: string[] = [];
-
-//   for (let i = 0; i < count; i++) {
-//     if (i < baseColors.length) {
-//       colors.push(baseColors[i]);
-//     } else {
-//       const r = Math.floor(Math.random() * 255);
-//       const g = Math.floor(Math.random() * 255);
-//       const b = Math.floor(Math.random() * 255);
-//       colors.push(`rgb(${r}, ${g}, ${b})`);
-//     }
-//   }
-
-//   return colors;
-// };
-
-// const PieChart = () => {
-//   const [subscriptionData, setSubscriptionData] = useState<SubscriptionData[]>(
-//     [],
-//   );
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         // 깡 userid 설정
-//         const userId = 1;
-//         const data = await getUserSubscription(userId);
-//         const formattedData = data.map((subscription: any) => ({
-//           label: subscription.ott.name,
-//           value: subscription.payment,
-//         }));
-//         setSubscriptionData(formattedData);
-//       } catch (error) {
-//         console.error('Error fetching subscription data:', error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   const data: ChartData<'pie', number[], string> = {
-//     labels: subscriptionData.map((item) => item.label),
-//     datasets: [
-//       {
-//         data: subscriptionData.map((item) => item.value),
-//         backgroundColor: generateColors(subscriptionData.length),
-//       },
-//     ],
-//   };
-
-//   const options: ChartOptions<'pie'> = {
-//     responsive: true,
-//     maintainAspectRatio: false,
-//     plugins: {
-//       legend: {
-//         position: 'bottom',
-//         align: 'start',
-//         labels: {
-//           boxWidth: 12,
-//           padding: 20,
-//           font: {
-//             size: 12,
-//           },
-//           generateLabels: (chart) => {
-//             const datasets = chart.data.datasets;
-//             if (datasets.length === 0) {
-//               return [];
-//             }
-//             const { data } = datasets[0];
-//             const total = (data as number[]).reduce((sum, val) => sum + val, 0);
-
-//             return (
-//               chart.data.labels?.map((label, i) => {
-//                 const value = data[i] as number;
-//                 const percentage = ((value / total) * 100).toFixed(2);
-//                 return {
-//                   text: `${label}: ${value}원 (${percentage}%)`,
-//                   fillStyle: Array.isArray(datasets[0].backgroundColor)
-//                     ? datasets[0].backgroundColor[i]
-//                     : undefined,
-//                   hidden: false,
-//                   index: i,
-//                 };
-//               }) || []
-//             );
-//           },
-//         },
-//       },
-//       tooltip: {
-//         callbacks: {
-//           label: function (context: TooltipItem<'pie'>) {
-//             const label = context.label || '';
-//             const value = context.raw as number;
-//             return `${label}: ${value}원`;
-//           },
-//         },
-//       },
-//     },
-//     layout: {
-//       padding: {
-//         top: 10,
-//         bottom: 10,
-//       },
-//     },
-//   };
-
-//   return (
-//     <S.ChartContainer>
-//       <Pie data={data} options={options} />
-//     </S.ChartContainer>
-//   );
-// };
-
-// export default PieChart;
-
-// api 활용 - 2: userid 로컬스토리지에서 가져옴
-// import React, { useState, useEffect } from 'react';
-// import { Pie } from 'react-chartjs-2';
-// import * as S from './PieChart.Style';
-// import { getUserSubscription } from '../../api/subscriptionApi';
-// import {
-//   Chart as ChartJS,
-//   ArcElement,
-//   Tooltip,
-//   Legend,
-//   ChartData,
-//   ChartOptions,
-//   TooltipItem,
-// } from 'chart.js';
-
-// ChartJS.register(ArcElement, Tooltip, Legend);
-
-// interface SubscriptionData {
-//   label: string;
-//   value: number;
-// }
-
-// const generateColors = (count: number): string[] => {
-//   const baseColors = [
-//     '#FF6384',
-//     '#36A2EB',
-//     '#FFCE56',
-//     '#4BC0C0',
-//     '#9966FF',
-//     '#FF9F40',
-//   ];
-//   const colors: string[] = [];
-
-//   for (let i = 0; i < count; i++) {
-//     if (i < baseColors.length) {
-//       colors.push(baseColors[i]);
-//     } else {
-//       const r = Math.floor(Math.random() * 255);
-//       const g = Math.floor(Math.random() * 255);
-//       const b = Math.floor(Math.random() * 255);
-//       colors.push(`rgb(${r}, ${g}, ${b})`);
-//     }
-//   }
-
-//   return colors;
-// };
-
-// const PieChart = () => {
-//   const [subscriptionData, setSubscriptionData] = useState<SubscriptionData[]>(
-//     [],
-//   );
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const userId = localStorage.getItem('userId'); // Get user ID from local storage
-//         if (userId) {
-//           const data = await getUserSubscription(Number(userId));
-//           const formattedData = data.map((subscription: any) => ({
-//             label: subscription.ott.name,
-//             value: subscription.payment,
-//           }));
-//           setSubscriptionData(formattedData);
-//         }
-//       } catch (error) {
-//         console.error('Error fetching subscription data:', error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   const data: ChartData<'pie', number[], string> = {
-//     labels: subscriptionData.map((item) => item.label),
-//     datasets: [
-//       {
-//         data: subscriptionData.map((item) => item.value),
-//         backgroundColor: generateColors(subscriptionData.length),
-//       },
-//     ],
-//   };
-
-//   const options: ChartOptions<'pie'> = {
-//     responsive: true,
-//     maintainAspectRatio: false,
-//     plugins: {
-//       legend: {
-//         position: 'bottom',
-//         align: 'start',
-//         labels: {
-//           boxWidth: 12,
-//           padding: 20,
-//           font: {
-//             size: 12,
-//           },
-//           generateLabels: (chart) => {
-//             const datasets = chart.data.datasets;
-//             if (datasets.length === 0) {
-//               return [];
-//             }
-//             const { data } = datasets[0];
-//             const total = (data as number[]).reduce((sum, val) => sum + val, 0);
-
-//             return (
-//               chart.data.labels?.map((label, i) => {
-//                 const value = data[i] as number;
-//                 const percentage = ((value / total) * 100).toFixed(2);
-//                 return {
-//                   text: `${label}: ${value}원 (${percentage}%)`,
-//                   fillStyle: Array.isArray(datasets[0].backgroundColor)
-//                     ? datasets[0].backgroundColor[i]
-//                     : undefined,
-//                   hidden: false,
-//                   index: i,
-//                 };
-//               }) || []
-//             );
-//           },
-//         },
-//       },
-//       tooltip: {
-//         callbacks: {
-//           label: function (context: TooltipItem<'pie'>) {
-//             const label = context.label || '';
-//             const value = context.raw as number;
-//             return `${label}: ${value}원`;
-//           },
-//         },
-//       },
-//     },
-//     layout: {
-//       padding: {
-//         top: 10,
-//         bottom: 10,
-//       },
-//     },
-//   };
-
-//   return (
-//     <S.ChartContainer>
-//       <Pie data={data} options={options} />
-//     </S.ChartContainer>
-//   );
-// };
-
-// export default PieChart;
-
 import React, { useState, useEffect } from 'react';
 import { Pie } from 'react-chartjs-2';
 import * as S from './PieChart.Style';
-import { getUserSubscription } from '../../api/subscriptionApi';
+import axios from 'axios';
 import {
   Chart as ChartJS,
   ArcElement,
@@ -451,15 +377,12 @@ import {
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 interface Subscription {
+  ottName: string;
   payment: number;
-  ott: {
-    name: string;
-  };
 }
 
-interface SubscriptionData {
-  label: string;
-  value: number;
+interface TotalPaymentResponse {
+  totalPayment: number;
 }
 
 const generateColors = (count: number): string[] => {
@@ -488,26 +411,22 @@ const generateColors = (count: number): string[] => {
 };
 
 const PieChart = () => {
-  const [subscriptionData, setSubscriptionData] = useState<SubscriptionData[]>(
-    [],
-  );
+  const [subscriptions, setSubscriptions] = useState<Subscription[]>([]);
+  const [totalPayment, setTotalPayment] = useState<number>(0);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const userId = localStorage.getItem('userId');
-        if (userId) {
-          const data: Subscription[] = await getUserSubscription(
-            Number(userId),
-          );
-          const formattedData = data.map((subscription) => ({
-            label: subscription.ott.name,
-            value: subscription.payment,
-          }));
-          setSubscriptionData(formattedData);
-        }
+        const [subscriptionsResponse, totalPaymentResponse] = await Promise.all(
+          [
+            axios.get<Subscription[]>('/api/subscription/user'),
+            axios.get<TotalPaymentResponse>('/api/subscription/total-payment'),
+          ],
+        );
+        setSubscriptions(subscriptionsResponse.data);
+        setTotalPayment(totalPaymentResponse.data.totalPayment);
       } catch (error) {
-        console.error('Error fetching subscription data:', error);
+        console.error('Error fetching data:', error);
       }
     };
 
@@ -515,11 +434,11 @@ const PieChart = () => {
   }, []);
 
   const data: ChartData<'pie', number[], string> = {
-    labels: subscriptionData.map((item) => item.label),
+    labels: subscriptions.map((sub) => sub.ottName),
     datasets: [
       {
-        data: subscriptionData.map((item) => item.value),
-        backgroundColor: generateColors(subscriptionData.length),
+        data: subscriptions.map((sub) => sub.payment),
+        backgroundColor: generateColors(subscriptions.length),
       },
     ],
   };
@@ -534,38 +453,33 @@ const PieChart = () => {
         labels: {
           boxWidth: 12,
           padding: 20,
-          font: { size: 12 },
-          generateLabels: (chart: ChartJS) => {
-            const datasets = chart.data.datasets;
-            if (datasets.length === 0) {
-              return [];
-            }
-            const data = datasets[0].data as number[];
-            const total = data.reduce((sum, val) => sum + val, 0);
-
-            return (
-              chart.data.labels?.map((label, i) => {
-                const value = data[i];
-                const percentage = ((value / total) * 100).toFixed(2);
-                return {
-                  text: `${label}: ${value}원 (${percentage}%)`,
-                  fillStyle: Array.isArray(datasets[0].backgroundColor)
-                    ? datasets[0].backgroundColor[i]
-                    : undefined,
-                  hidden: false,
-                  index: i,
-                };
-              }) || []
-            );
+          font: {
+            size: 12,
+          },
+          generateLabels: (chart) => {
+            return subscriptions.map((sub, i) => {
+              const percentage = ((sub.payment / totalPayment) * 100).toFixed(
+                2,
+              );
+              return {
+                text: `${sub.ottName}: ${sub.payment}원 (${percentage}%)`,
+                fillStyle: Array.isArray(chart.data.datasets[0].backgroundColor)
+                  ? chart.data.datasets[0].backgroundColor[i]
+                  : undefined,
+                hidden: false,
+                index: i,
+              };
+            });
           },
         },
       },
       tooltip: {
         callbacks: {
-          label: (context: TooltipItem<'pie'>) => {
+          label: function (context: TooltipItem<'pie'>) {
             const label = context.label || '';
             const value = context.raw as number;
-            return `${label}: ${value}원`;
+            const percentage = ((value / totalPayment) * 100).toFixed(2);
+            return `${label}: ${value}원 (${percentage}%)`;
           },
         },
       },
