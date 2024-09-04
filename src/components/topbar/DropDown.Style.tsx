204c16c1cc36476d31decf3479e7672a5946e024
KimJuha <kimjuha@KimJuhaui-MacBookPro.local>
2024-09-04 17:05:24 +0900

    Feat: 커뮤니티 글 리스트 컴포넌트 1차 작성

---
+++ src/components/topbar/DropDown.Style.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Dropdown = styled.div`
+  margin: 16px 0;
+  padding: 0 16px;
+`;
+
+export const DropdownSelect = styled.select`
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  font-size: 16px;
+  appearance: none;
+  background-color: #f8f8f8;
+  outline: none;
+  cursor: pointer;
+`;
--- src/pages/Community/Community.tsx
+++ src/pages/Community/Community.tsx
@@ -1,22 +1,50 @@
-import React from 'react';
+// import React from 'react';
+// import Search from '../../components/Search/Search';
+// import CommunityList from '../../components/CommunityList/CommunityList';
+// import BottomNavBar from '../../components/BottomBar/BottomNavBar';
+// import * as S from './Community.Style';
+
+// const Community = () => {
+//   return (
+//     <div>
+//       <Search />
+//       <CommunityList />
+//       <S.BottomNavBarWrapper>
+//         <BottomNavBar />
+//       </S.BottomNavBarWrapper>
+//     </div>
+//   );
+// };
+
+// export default Community;
+
 import Search from '../../components/Search/Search';
 import CommunityList from '../../components/CommunityList/CommunityList';
+import DropDown from '../../components/TopBar/DropDown';
+import ActionButton from '../../components/common/ActionButton';
 import BottomNavBar from '../../components/BottomBar/BottomNavBar';
 import * as S from './Community.Style';
 
 const Community = () => {
+  const handleCreatePost = () => {
+    window.location.href = '/create-post';
+  };
+
+  const ottOptions = [
+    '넷플릭스',
+    '티빙',
+    '웨이브',
+    '디즈니+',
+    '쿠팡플레이',
+    '왓챠',
+  ];
+
   return (
     <div>
-      <S.Dropdown>
-        <S.DropdownSelect>
-          <option>모든 OTT</option>
-          <option>넷플릭스</option>
-          <option>티빙</option>
-          <option>왓챠</option>
-        </S.DropdownSelect>
-      </S.Dropdown>
+      <DropDown options={ottOptions} />
       <Search />
       <CommunityList />
+      <ActionButton text="글 작성" onClick={handleCreatePost} />
       <S.BottomNavBarWrapper>
         <BottomNavBar />
       </S.BottomNavBarWrapper>
