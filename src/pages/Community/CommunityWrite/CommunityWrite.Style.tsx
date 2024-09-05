216e7b6768a2758d373683411bccdb267c47edc7
KimJuha <kimjuha@KimJuhaui-MacBookPro.local>
2024-09-05 09:43:52 +0900

    Fix: 글작성 규격 통일

--- src/pages/Community/CommunityWrite/CommunityWrite.Style.tsx
+++ src/pages/Community/CommunityWrite/CommunityWrite.Style.tsx
@@ -1,8 +1,21 @@
 import styled from 'styled-components';
 
 export const CommunityWrite = styled.div`
-  display: flex;
+  // display: flex;
   flex-direction: column;
   height: 100vh;
   background-color: #ffffff;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 375px;
+  height: 100vh;
+  padding: 0 20px;
+  box-sizing: border-box;
+  margin-top: 10px;
+  overflow: hidden;
+  overflow-y: auto;
+  margin-bottom: 85px;
+  z-index: 50;
 `;
