25c6493ed4c85839dee2110e4c33f53404b95f49
KimJuha <kimjuha@KimJuhaui-MacBookPro.local>
2024-09-05 09:17:25 +0900

    fix: 글쓰기 컴포넌트 임포트 수정

---
+++ src/components/Community/PotSelect/PotSelect.Style.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const PotSelect = styled.div`
+  margin: 20px 16px;
+`;
+
+export const Select = styled.select`
+  width: 100%;
+  padding: 10px;
+  font-size: 14px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  outline: none;
+  background-color: #f9f9f9;
+
+  &:focus {
+    border-color: #888;
+  }
+`;
--- src/pages/Community/Community.tsx
+++ src/pages/Community/Community.tsx
@@ -1,3 +1,43 @@
+// import Search from '../../components/Search/Search';
+// import CommunityList from '../../components/Community/CommunityList/CommunityList';
+// import DropDown from '../../components/TopBar/DropDown';
+// import ActionButton from '../../components/common/ActionButton';
+// import BottomNavBar from '../../components/BottomBar/BottomNavBar';
+// import * as S from './Community.Style';
+
+// const Community = () => {
+//   const handleCreatePost = () => {
+//     window.location.href = '/community-write';
+//   };
+
+//   const ottOptions = [
+//     '넷플릭스',
+//     '티빙',
+//     '웨이브',
+//     '디즈니+',
+//     '쿠팡플레이',
+//     '왓챠',
+//   ];
+
+//   return (
+//     <div>
+//       <DropDown options={ottOptions} />
+//       <Search />
+//       <CommunityList />
+//       <ActionButton text="글 작성" onClick={handleCreatePost} />
+//       <S.BottomNavBarWrapper>
+//         <BottomNavBar />
+//       </S.BottomNavBarWrapper>
+//     </div>
+//   );
+// };
+
+// export default Community;
+
+// pages/Community/Community.tsx
+
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Search from '../../components/Search/Search';
 import CommunityList from '../../components/Community/CommunityList/CommunityList';
 import DropDown from '../../components/TopBar/DropDown';
@@ -6,8 +46,11 @@ import BottomNavBar from '../../components/BottomBar/BottomNavBar';
 import * as S from './Community.Style';
 
 const Community = () => {
+  const navigate = useNavigate(); // useNavigate 훅 사용
+
+  // 글 작성 버튼 클릭 시 CommunityWrite 페이지로 이동
   const handleCreatePost = () => {
-    window.location.href = '/community-write';
+    navigate('/community-write'); // 변경된 경로로 이동
   };
 
   const ottOptions = [
