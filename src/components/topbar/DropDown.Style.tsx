2982e719be38dda76b9d87a58fc7dfbeeb67f0ee
KimJuha <kimjuha@KimJuhaui-MacBookPro.local>
2024-09-05 11:00:26 +0900

    Fix: 드롭박스 규격 수정

--- src/components/topbar/DropDown.Style.tsx
+++ src/components/topbar/DropDown.Style.tsx
@@ -3,18 +3,32 @@ import styled from 'styled-components';
 export const Dropdown = styled.div`
   display: flex;
   align-items: center;
+  border-bottom: 0.5px solid black;
 `;
 
+// export const DropdownSelect = styled.select`
+//   width: 100%;
+//   padding-left: 20px;
+//   border-bottom: 0.5px solid black;
+//   font-size: 20px;
+//   padding-top: 36px;
+//   padding-bottom: 20px;
+//   appearance: none;
+//   background-color: #fff;
+//   outline: none;
+//   cursor: pointer;
+//   font-weight: 400;
+// `;
+
 export const DropdownSelect = styled.select`
-  width: 100%;
+  border: none;
   padding-left: 20px;
-  border-bottom: 0.5px solid black;
+  border: none;
   font-size: 20px;
   padding-top: 36px;
   padding-bottom: 20px;
-  appearance: none;
-  background-color: #fff;
   outline: none;
   cursor: pointer;
   font-weight: 400;
+  background-color: #fff;
 `;
--- src/components/topbar/DropDown.tsx
+++ src/components/topbar/DropDown.tsx
@@ -27,3 +27,33 @@ const DropDown: React.FC<DropDownProps> = ({
 };
 
 export default DropDown;
+
+// import React from 'react';
+// import * as S from './DropDown.Style';
+
+// interface DropDownProps {
+//   options: string[];
+//   onChange?: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+//   defaultOption?: string;
+// }
+
+// const DropDown: React.FC<DropDownProps> = ({
+//   options,
+//   onChange,
+//   defaultOption = '모든 OTT',
+// }) => {
+//   return (
+//     <S.DropdownWrapper>
+//       <S.DefaultText>{defaultOption}</S.DefaultText>
+//       <S.DropdownSelect onChange={onChange}>
+//         {options.map((option, index) => (
+//           <option key={index} value={option}>
+//             {option}
+//           </option>
+//         ))}
+//       </S.DropdownSelect>
+//     </S.DropdownWrapper>
+//   );
+// };
+
+// export default DropDown;
