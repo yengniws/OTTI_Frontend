e552a7aa393ecd39f23439471364ab12e0db1079
KimJuha <kimjuha@KimJuhaui-MacBookPro.local>
2024-09-06 15:31:21 +0900

    Fix: 규격 수정

--- src/pages/Community/CommunityDetail/CommentDetail.Style.tsx
+++ src/pages/Community/CommunityDetail/CommentDetail.Style.tsx
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  // align-items: center;
   width: 100%;
   max-width: 375px;
   height: 100vh;
+  margin: 0 auto;
 `;
 
 export const TitleWrapper = styled.header`
@@ -17,5 +18,16 @@ export const TitleWrapper = styled.header`
 `;
 
 export const CommunityPostWrapper = styled.div`
+  // padding: 0px 20px;
   border-bottom: 1px solid #ddd;
+`;
+
+export const PageWrapper = styled.div`
+  max-width: 375px;
+  box-sizing: border-box;
+  padding: 0px 20px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  // align-items: center;
 `;
--- src/pages/Community/CommunityDetail/CommunityDetail.tsx
+++ src/pages/Community/CommunityDetail/CommunityDetail.tsx
@@ -4,7 +4,7 @@ import CommunityPost from '../../../components/Community/CommunityPost/Community
 import AddComment from '../../../components/Community/Comment/AddComment';
 import CommentList from '../../../components/Community/Comment/CommentList';
 import JoinBtn from '../../../components/common/JoinBtn/JoinBtn';
-import TopBar from '../../../components/TopBar/NewTopBar';
+import NewTopBar from '../../../components/TopBar/NewTopBar';
 
 interface Comment {
   id: number;
@@ -28,24 +28,26 @@ const CommunityDetail: React.FC = () => {
   return (
     <S.Container>
       <S.TitleWrapper>
-        <TopBar title="커뮤니티" />
+        <NewTopBar title="커뮤니티" />
       </S.TitleWrapper>
-      <S.CommunityPostWrapper>
-        <CommunityPost
-          author="닉네임1"
-          createdAt="3분 전"
-          title="넷플릭스 1자리 구해요"
-          content="프리미엄 요금제 1명 자리 남았습니다. 저희는 현재 3명이 함께 이용 중이며, 넷플릭스의 프리미엄 요금제를 공유하고 있습니다. 한 달에 4,500원의 비용으로 고화질 스트리밍을 즐기실 수 있습니다. 관심 있으신 분은 댓글이나 쪽지로 연락 주세요!"
-        />
+      <S.PageWrapper>
+        <S.CommunityPostWrapper>
+          <CommunityPost
+            author="닉네임1"
+            createdAt="3분 전"
+            title="넷플릭스 1자리 구해요"
+            content="프리미엄 요금제 1명 자리 남았습니다. "
+          />
 
-        <JoinBtn text="참여하기" />
-      </S.CommunityPostWrapper>
+          <JoinBtn text="참여하기" />
+        </S.CommunityPostWrapper>
 
-      {/* 댓글 추가 기능 */}
-      <AddComment onAddComment={handleAddComment} />
+        {/* 댓글 추가 기능 */}
+        <AddComment onAddComment={handleAddComment} />
 
-      {/* 댓글 리스트 */}
-      <CommentList comments={comments} />
+        {/* 댓글 리스트 */}
+        <CommentList comments={comments} />
+      </S.PageWrapper>
     </S.Container>
   );
 };
