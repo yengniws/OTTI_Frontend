06d99c57f245681807c4e958aa11d6fa83eca09b
KimJuha <kimjuha@KimJuhaui-MacBookPro.local>
2024-09-21 02:00:29 +0900

    Fix: 오류 수정

--- src/pages/Community/CommunityDetail/CommunityDetail.tsx
+++ src/pages/Community/CommunityDetail/CommunityDetail.tsx
@@ -1,107 +1,107 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import axiosInstance from '../../../libs/AxiosInstance';
-import * as S from './CommunityDetail.Style';
-import CommunityPost from '../../../components/Community/CommunityPost/CommunityPost';
-import AddComment from '../../../components/Community/Comment/AddComment';
-import CommentList from '../../../components/Community/Comment/CommentList';
-import JoinBtn from '../../../components/common/JoinBtn/JoinBtn';
-import NewTopBar from '../../../components/TopBar/NewTopBar';
+// import React, { useEffect, useState } from 'react';
+// import { useParams } from 'react-router-dom';
+// import axiosInstance from '../../../libs/AxiosInstance';
+// import * as S from './CommunityDetail.Style';
+// import CommunityPost from '../../../components/Community/CommunityPost/CommunityPost';
+// import AddComment from '../../../components/Community/Comment/AddComment';
+// import CommentList from '../../../components/Community/Comment/CommentList';
+// import JoinBtn from '../../../components/common/JoinBtn/JoinBtn';
+// import NewTopBar from '../../../components/TopBar/NewTopBar';
 
-interface Comment {
-  id: number;
-  username: string;
-  content: string;
-  createdAt: string;
-  profilePhotoUrl: string;
-}
+// interface Comment {
+//   id: number;
+//   username: string;
+//   content: string;
+//   createdAt: string;
+//   profilePhotoUrl: string;
+// }
 
-interface Post {
-  id: number;
-  username: string;
-  createdAt: string;
-  title: string;
-  content: string;
-  potId: number; // potId 추가 -> 해당하는 팟에 대한 정보 가져오기
-  comments: Comment[];
-}
+// interface Post {
+//   id: number;
+//   username: string;
+//   createdAt: string;
+//   title: string;
+//   content: string;
+//   potId: number; // potId 추가 -> 해당하는 팟에 대한 정보 가져오기
+//   comments: Comment[];
+// }
 
-const defaultProfileImage =
-  'https://otti-bucket-2024.s3.ap-northeast-2.amazonaws.com/otti-image/otti.png';
+// const defaultProfileImage =
+//   'https://otti-bucket-2024.s3.ap-northeast-2.amazonaws.com/otti-image/otti.png';
 
-const CommunityDetail: React.FC = () => {
-  const { id } = useParams<{ id: string }>();
-  const [post, setPost] = useState<Post | null>(null);
-  const [comments, setComments] = useState<Comment[]>([]);
-  const postId = Number(id);
+// const CommunityDetail: React.FC = () => {
+//   const { id } = useParams<{ id: string }>();
+//   const [post, setPost] = useState<Post | null>(null);
+//   const [comments, setComments] = useState<Comment[]>([]);
+//   const postId = Number(id);
 
-  useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        const response = await axiosInstance.get(`/api/post/${postId}`);
-        setPost(response.data);
-        setComments(response.data.comments || []);
-      } catch (error) {
-        console.error('Failed to fetch post', error);
-      }
-    };
+//   useEffect(() => {
+//     const fetchPost = async () => {
+//       try {
+//         const response = await axiosInstance.get(`/api/post/${postId}`);
+//         setPost(response.data);
+//         setComments(response.data.comments || []);
+//       } catch (error) {
+//         console.error('Failed to fetch post', error);
+//       }
+//     };
 
-    if (postId) {
-      fetchPost();
-    }
-  }, [postId]);
+//     if (postId) {
+//       fetchPost();
+//     }
+//   }, [postId]);
 
-  const handleAddComment = async (content: string) => {
-    try {
-      const response = await axiosInstance.post('/api/post/comment', {
-        text: content,
-        post: postId,
-      });
+//   const handleAddComment = async (content: string) => {
+//     try {
+//       const response = await axiosInstance.post('/api/post/comment', {
+//         text: content,
+//         post: postId,
+//       });
 
-      const newComment = {
-        id: response.data.id,
-        username: response.data.userName.userName,
-        content: response.data.text,
-        createdAt: response.data.createdDate,
-        profilePhotoUrl:
-          response.data.userName.userprofilePhotoUrl || defaultProfileImage,
-      };
+//       const newComment = {
+//         id: response.data.id,
+//         username: response.data.userName.userName,
+//         content: response.data.text,
+//         createdAt: response.data.createdDate,
+//         profilePhotoUrl:
+//           response.data.userName.userprofilePhotoUrl || defaultProfileImage,
+//       };
 
-      setComments([...comments, newComment]);
-    } catch (error) {
-      console.error('Failed to add comment', error);
-    }
-  };
+//       setComments([...comments, newComment]);
+//     } catch (error) {
+//       console.error('Failed to add comment', error);
+//     }
+//   };
 
-  if (!post) {
-    return <div>Loading...</div>;
-  }
+//   if (!post) {
+//     return <div>Loading...</div>;
+//   }
 
-  return (
-    <S.Container>
-      <S.TitleWrapper>
-        <NewTopBar title="커뮤니티" />
-      </S.TitleWrapper>
-      <S.PageWrapper>
-        <S.CommunityPostWrapper>
-          <CommunityPost
-            postId={postId}
-            userName={post.username}
-            createdAt={post.createdAt}
-            title={post.title}
-            content={post.content}
-          />
-          {/* post의 potId를 JoinBtn에 전달 */}
-          <JoinBtn potId={post.potId} />
-        </S.CommunityPostWrapper>
-        <AddComment postId={postId} onAddComment={handleAddComment} />
-        <CommentList comments={comments} />
-      </S.PageWrapper>
-    </S.Container>
-  );
-};
+//   return (
+//     <S.Container>
+//       <S.TitleWrapper>
+//         <NewTopBar title="커뮤니티" />
+//       </S.TitleWrapper>
+//       <S.PageWrapper>
+//         <S.CommunityPostWrapper>
+//           <CommunityPost
+//             postId={postId}
+//             // userName={post.username}
+//             // createdAt={post.createdAt}
+//             // title={post.title}
+//             // content={post.content}
+//           />
+//           <JoinBtn potId={post.potId} />
+//         </S.CommunityPostWrapper>
+//         <AddComment postId={postId} onAddComment={handleAddComment} />
+//         {/* <CommentList comments={comments} /> */}
+//         <CommentList comments={comments} />
+//       </S.PageWrapper>
+//     </S.Container>
+//   );
+// };
 
-export default CommunityDetail;
+// export default CommunityDetail;
 
 // import React, { useEffect, useState } from 'react';
 // import { useParams } from 'react-router-dom';
@@ -129,7 +129,7 @@ export default CommunityDetail;
 //   content: string;
 //   potId: number;
 //   comments: Comment[];
-//   viewCount: number;  // 조회수 필드 추가
+//   viewCount: number;
 // }
 
 // const defaultProfileImage =
@@ -214,3 +214,107 @@ export default CommunityDetail;
 // };
 
 // export default CommunityDetail;
+
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axiosInstance from '../../../libs/AxiosInstance';
+import * as S from './CommunityDetail.Style';
+import CommunityPost from '../../../components/Community/CommunityPost/CommunityPost';
+import AddComment from '../../../components/Community/Comment/AddComment';
+import CommentList from '../../../components/Community/Comment/CommentList';
+import JoinBtn from '../../../components/common/JoinBtn/JoinBtn';
+import NewTopBar from '../../../components/TopBar/NewTopBar';
+
+interface CommentUserInfo {
+  userName: string;
+  userprofilePhotoUrl: string;
+}
+
+interface Comment {
+  id: number;
+  text: string;
+  userName: CommentUserInfo;
+  createdDate: string;
+}
+
+interface Post {
+  id: number;
+  username: string;
+  createdAt: string;
+  title: string;
+  content: string;
+  potId: number;
+  comments: Comment[];
+}
+
+const defaultProfileImage =
+  'https://otti-bucket-2024.s3.ap-northeast-2.amazonaws.com/otti-image/otti.png';
+
+const CommunityDetail: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  const [post, setPost] = useState<Post | null>(null);
+  const [comments, setComments] = useState<Comment[]>([]);
+  const postId = Number(id);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const response = await axiosInstance.get(`/api/post/${postId}`);
+        setPost(response.data);
+        setComments(response.data.comments || []);
+      } catch (error) {
+        console.error('Failed to fetch post', error);
+      }
+    };
+
+    if (postId) {
+      fetchPost();
+    }
+  }, [postId]);
+
+  const handleAddComment = async (content: string) => {
+    try {
+      const response = await axiosInstance.post('/api/post/comment', {
+        text: content,
+        post: postId,
+      });
+
+      const newComment = {
+        id: response.data.id,
+        text: response.data.text,
+        userName: {
+          userName: response.data.userName.userName,
+          userprofilePhotoUrl:
+            response.data.userName.userprofilePhotoUrl || defaultProfileImage,
+        },
+        createdDate: response.data.createdDate,
+      };
+
+      setComments([...comments, newComment]);
+    } catch (error) {
+      console.error('Failed to add comment', error);
+    }
+  };
+
+  if (!post) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <S.Container>
+      <S.TitleWrapper>
+        <NewTopBar title="커뮤니티" />
+      </S.TitleWrapper>
+      <S.PageWrapper>
+        <S.CommunityPostWrapper>
+          <CommunityPost postId={postId} />
+          <JoinBtn potId={post.potId} />
+        </S.CommunityPostWrapper>
+        <AddComment postId={postId} onAddComment={handleAddComment} />
+        <CommentList comments={comments} />
+      </S.PageWrapper>
+    </S.Container>
+  );
+};
+
+export default CommunityDetail;
