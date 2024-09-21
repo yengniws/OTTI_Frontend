3835b1e127824c1a8a388a71734f4105724e5ce4
KimJuha <kimjuha@KimJuhaui-MacBookPro.local>
2024-09-22 03:45:50 +0900

    Fix: 중간저장

--- src/pages/Community/CommunityDetail/CommunityDetail.tsx
+++ src/pages/Community/CommunityDetail/CommunityDetail.tsx
@@ -1,108 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import { useParams } from 'react-router-dom';
-// import axiosInstance from '../../../libs/AxiosInstance';
-// import * as S from './CommunityDetail.Style';
-// import CommunityPost from '../../../components/Community/CommunityPost/CommunityPost';
-// import AddComment from '../../../components/Community/Comment/AddComment';
-// import CommentList from '../../../components/Community/Comment/CommentList';
-// import JoinBtn from '../../../components/common/JoinBtn/JoinBtn';
-// import NewTopBar from '../../../components/TopBar/NewTopBar';
-
-// interface CommentUserInfo {
-//   userName: string;
-//   userprofilePhotoUrl: string;
-// }
-
-// interface Comment {
-//   id: number;
-//   text: string;
-//   createdDate: string;
-//   userInfo: CommentUserInfo;
-// }
-
-// interface Post {
-//   id: number;
-//   username: string;
-//   createdAt: string;
-//   title: string;
-//   content: string;
-//   potId: number;
-//   comments: Comment[];
-// }
-
-// const defaultProfileImage =
-//   'https://otti-bucket-2024.s3.ap-northeast-2.amazonaws.com/otti-image/otti.png';
-
-// const CommunityDetail: React.FC = () => {
-//   const { id } = useParams<{ id: string }>();
-//   const [post, setPost] = useState<Post | null>(null);
-//   const [comments, setComments] = useState<Comment[]>([]);
-//   const postId = Number(id);
-
-//   useEffect(() => {
-//     const fetchPost = async () => {
-//       try {
-//         const response = await axiosInstance.get(`/api/post/${postId}`);
-//         setPost(response.data);
-//         setComments(response.data.comments || []);
-//       } catch (error) {
-//         console.error('Failed to fetch post', error);
-//       }
-//     };
-
-//     if (postId) {
-//       fetchPost();
-//     }
-//   }, [postId]);
-
-//   const handleAddComment = async (content: string) => {
-//     try {
-//       const response = await axiosInstance.post('/api/post/comment', {
-//         text: content,
-//         post: postId,
-//       });
-
-//       const newComment: Comment = {
-//         id: response.data.id,
-//         text: response.data.text,
-//         createdDate: response.data.createdDate,
-//         userInfo: {
-//           userName: response.data.userName.userName,
-//           userprofilePhotoUrl:
-//             response.data.userName.userprofilePhotoUrl || defaultProfileImage,
-//         },
-//       };
-
-//       setComments([...comments, newComment]);
-//     } catch (error) {
-//       console.error('Failed to add comment', error);
-//     }
-//   };
-
-//   if (!post) {
-//     return <div>Loading...</div>;
-//   }
-
-//   return (
-//     <S.Container>
-//       <S.TitleWrapper>
-//         <NewTopBar title="커뮤니티" />
-//       </S.TitleWrapper>
-//       <S.PageWrapper>
-//         <S.CommunityPostWrapper>
-//           <CommunityPost postId={postId} />
-//           <JoinBtn potId={post.potId} />
-//         </S.CommunityPostWrapper>
-//         <CommentList comments={comments} />
-
-//         <AddComment postId={postId} onAddComment={handleAddComment} />
-//       </S.PageWrapper>
-//     </S.Container>
-//   );
-// };
-
-// export default CommunityDetail;
-
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axiosInstance from '../../../libs/AxiosInstance';
@@ -144,20 +39,16 @@ const CommunityDetail: React.FC = () => {
   const [comments, setComments] = useState<Comment[]>([]);
   const postId = Number(id);
 
-  console.log(comments);
+  // console.log(comments);
+
   useEffect(() => {
     const fetchPost = async () => {
       try {
         const response = await axiosInstance.get(`/api/post/${postId}`);
-        // console.log('Post data:', response.data);
         setPost(response.data);
         setComments(response.data.comments || []);
 
-        response.data.comments?.forEach((comment: Comment) => {
-          // console.log('Comment:', comment);
-          // console.log('Username:', comment.userInfo.userName);
-          // console.log('Profile URL:', comment.userInfo.userprofilePhotoUrl);
-        });
+        response.data.comments?.forEach((comment: Comment) => {});
       } catch (error) {
         console.error('Failed to fetch post', error);
       }
@@ -174,6 +65,7 @@ const CommunityDetail: React.FC = () => {
         text: content,
         post: postId,
       });
+      console.log(response);
 
       const newComment: Comment = {
         id: response.data.id,
@@ -182,7 +74,7 @@ const CommunityDetail: React.FC = () => {
         userInfo: {
           userName: response.data.userInfo.userName,
           userprofilePhotoUrl:
-            response.data.uuserInfo.userprofilePhotoUrl || defaultProfileImage,
+            response.data.userInfo.userprofilePhotoUrl || defaultProfileImage,
         },
       };
 
@@ -209,8 +101,8 @@ const CommunityDetail: React.FC = () => {
           <CommunityPost postId={postId} />
           <JoinBtn potId={post.potId} />
         </S.CommunityPostWrapper>
-        <CommentList comments={comments} />
         <AddComment postId={postId} onAddComment={handleAddComment} />
+        <CommentList comments={comments} />
       </S.PageWrapper>
     </S.Container>
   );
