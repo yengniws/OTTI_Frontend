aa2c0fdf9cba64d9a6a010d1e2f26711a7d9c798
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-09-23 05:01:26 +0900

    Fix: 주석

--- src/pages/Main/SubscriptionFeeDetail.tsx
+++ src/pages/Main/SubscriptionFeeDetail.tsx
@@ -20,7 +20,7 @@ const SubscriptionFeeDetail: React.FC = () => {
         <NewTopBar title="이번 달 총 구독료" />
       </S.TitleWrapper>
       <S.Container>
-        <TotalSubscriptionFee initialTotalAmount={totalAmount} />
+        {/* <TotalSubscriptionFee initialTotalAmount={totalAmount} /> */}
         <S.PieChartTitle>한 눈에 보기</S.PieChartTitle>
         <S.DetailContainer>
           <PieChart />
