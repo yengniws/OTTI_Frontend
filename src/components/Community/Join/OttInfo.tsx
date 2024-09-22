9616b632f6700b4e136a57726156e33cd17c06b9
KimJuha <kimjuha@KimJuhaui-MacBookPro.local>
2024-09-23 02:02:09 +0900

    Fix: 팟 주석 제거

--- src/components/Community/Join/OttInfo.tsx
+++ src/components/Community/Join/OttInfo.tsx
@@ -1,63 +1,13 @@
-// import React from 'react';
-// import * as S from './OttInfo.Style';
-
-// interface OttInfoProps {
-//   imageUrl: string;
-//   ottname: string;
-//   plan: string;
-//   price: string;
-//   paymentDate: string;
-//   currentMembers: string;
-// }
-
-// const OttInfo = ({
-//   imageUrl,
-//   ottname,
-//   plan,
-//   price,
-//   paymentDate,
-//   currentMembers,
-// }: OttInfoProps) => {
-//   return (
-//     <S.OttInfoWrap>
-//       <S.Header>
-//         <S.OttImage src={imageUrl} alt={`${ottname} 로고`} />
-//         <S.OttName>{ottname}</S.OttName>
-//       </S.Header>
-//       <S.OttDetails>
-//         <S.DetailRow>
-//           <S.Label>요금제</S.Label>
-//           <S.OttText>{plan}</S.OttText>
-//         </S.DetailRow>
-//         <S.DetailRow>
-//           <S.Label>가격</S.Label>
-//           <S.OttText>{price}</S.OttText>
-//         </S.DetailRow>
-//         <S.DetailRow>
-//           <S.Label>납부일</S.Label>
-//           <S.OttText>{paymentDate}</S.OttText>
-//         </S.DetailRow>
-//         <S.DetailRow>
-//           <S.Label>현재 팟 인원</S.Label>
-//           <S.OttText>{currentMembers}</S.OttText>
-//         </S.DetailRow>
-//       </S.OttDetails>
-//     </S.OttInfoWrap>
-//   );
-// };
-
-// export default OttInfo;
-
 import React from 'react';
 import * as S from './OttInfo.Style';
 
 interface OttInfoProps {
-  image: string; // ott.image
-  name: string; // ott.name (OttName)
-  ratePlan: string; // ott.ratePlan (Plan for Ott)
-  price: number; // ott.price (Price)
-  paymentDate: string; // outer ratePlan (PaymentDate)
-  memberCount: number; // outer memberCount (CurrentMembers)
+  image: string;
+  name: string;
+  ratePlan: string;
+  price: number;
+  paymentDate: string;
+  memberCount: number;
 }
 
 const OttInfo = ({
