ecdfbf8d2335b268b2033b2e324407a425908097
KimJuha <kimjuha@KimJuhaui-MacBookPro.local>
2024-09-23 21:09:51 +0900

    fix: 도르마무

--- src/components/Community/Join/OttInfo.tsx
+++ src/components/Community/Join/OttInfo.tsx
@@ -1,34 +1,85 @@
+// import React from 'react';
+// import * as S from './OttInfo.Style';
+
+// interface OttProps {
+//   image: string;
+//   name: string;
+//   ratePlan: string;
+//   price: number;
+//   memberCount: number;
+// }
+
+// interface OttInfoProps {
+//   ott: OttProps; // ott 정보를 props로 받음
+//   paymentDate: string;
+// }
+
+// const OttInfo = ({ ott, paymentDate }: OttInfoProps) => {
+//   return (
+//     <S.OttInfoWrap>
+//       <S.Header>
+//         <S.OttImage src={ott.image} alt={`${ott.name} 로고`} />
+//         <S.OttName>{ott.name}</S.OttName>
+//       </S.Header>
+//       <S.OttDetails>
+//         <S.DetailRow>
+//           <S.Label>요금제</S.Label>
+//           <S.OttText>{ott.ratePlan}</S.OttText>
+//         </S.DetailRow>
+//         <S.DetailRow>
+//           <S.Label>가격</S.Label>
+//           <S.OttText>{ott.price}원</S.OttText>
+//         </S.DetailRow>
+//         <S.DetailRow>
+//           <S.Label>납부일</S.Label>
+//           <S.OttText>{paymentDate}</S.OttText>
+//         </S.DetailRow>
+//         <S.DetailRow>
+//           <S.Label>현재 팟 인원</S.Label>
+//           <S.OttText>{ott.memberCount}명</S.OttText>
+//         </S.DetailRow>
+//       </S.OttDetails>
+//     </S.OttInfoWrap>
+//   );
+// };
+
+// export default OttInfo;
+
 import React from 'react';
 import * as S from './OttInfo.Style';
 
-interface OttProps {
+interface OttInfoProps {
   image: string;
   name: string;
   ratePlan: string;
   price: number;
   memberCount: number;
-}
-
-interface OttInfoProps {
-  ott: OttProps; // ott 정보를 props로 받음
   paymentDate: string;
+
 }
 
-const OttInfo = ({ ott, paymentDate }: OttInfoProps) => {
+const OttInfo = ({
+  image,
+  name,
+  ratePlan,
+  price,
+  paymentDate,
+  memberCount,
+}: OttInfoProps) => {
   return (
     <S.OttInfoWrap>
       <S.Header>
-        <S.OttImage src={ott.image} alt={`${ott.name} 로고`} />
-        <S.OttName>{ott.name}</S.OttName>
+        <S.OttImage src={image} alt={${name} 로고} />
+        <S.OttName>{name}</S.OttName>
       </S.Header>
       <S.OttDetails>
         <S.DetailRow>
           <S.Label>요금제</S.Label>
-          <S.OttText>{ott.ratePlan}</S.OttText>
+          <S.OttText>{ratePlan}</S.OttText>
         </S.DetailRow>
         <S.DetailRow>
           <S.Label>가격</S.Label>
-          <S.OttText>{ott.price}원</S.OttText>
+          <S.OttText>{price}원</S.OttText>
         </S.DetailRow>
         <S.DetailRow>
           <S.Label>납부일</S.Label>
@@ -36,7 +87,7 @@ const OttInfo = ({ ott, paymentDate }: OttInfoProps) => {
         </S.DetailRow>
         <S.DetailRow>
           <S.Label>현재 팟 인원</S.Label>
-          <S.OttText>{ott.memberCount}명</S.OttText>
+          <S.OttText>{memberCount}명</S.OttText>
         </S.DetailRow>
       </S.OttDetails>
     </S.OttInfoWrap>
