f3a30c362d28931d0344a138e450968583fc4edd
KimJuha <kimjuha@KimJuhaui-MacBookPro.local>
2024-09-23 20:59:12 +0900

    Fix: 팟 정보 수정중

--- src/components/Community/Join/OttInfo.tsx
+++ src/components/Community/Join/OttInfo.tsx
@@ -1,37 +1,34 @@
 import React from 'react';
 import * as S from './OttInfo.Style';
 
-interface OttInfoProps {
+interface OttProps {
   image: string;
   name: string;
   ratePlan: string;
   price: number;
-  paymentDate: string;
   memberCount: number;
 }
 
-const OttInfo = ({
-  image,
-  name,
-  ratePlan,
-  price,
-  paymentDate,
-  memberCount,
-}: OttInfoProps) => {
+interface OttInfoProps {
+  ott: OttProps; // ott 정보를 props로 받음
+  paymentDate: string;
+}
+
+const OttInfo = ({ ott, paymentDate }: OttInfoProps) => {
   return (
     <S.OttInfoWrap>
       <S.Header>
-        <S.OttImage src={image} alt={`${name} 로고`} />
-        <S.OttName>{name}</S.OttName>
+        <S.OttImage src={ott.image} alt={`${ott.name} 로고`} />
+        <S.OttName>{ott.name}</S.OttName>
       </S.Header>
       <S.OttDetails>
         <S.DetailRow>
           <S.Label>요금제</S.Label>
-          <S.OttText>{ratePlan}</S.OttText>
+          <S.OttText>{ott.ratePlan}</S.OttText>
         </S.DetailRow>
         <S.DetailRow>
           <S.Label>가격</S.Label>
-          <S.OttText>{price}원</S.OttText>
+          <S.OttText>{ott.price}원</S.OttText>
         </S.DetailRow>
         <S.DetailRow>
           <S.Label>납부일</S.Label>
@@ -39,7 +36,7 @@ const OttInfo = ({
         </S.DetailRow>
         <S.DetailRow>
           <S.Label>현재 팟 인원</S.Label>
-          <S.OttText>{memberCount}명</S.OttText>
+          <S.OttText>{ott.memberCount}명</S.OttText>
         </S.DetailRow>
       </S.OttDetails>
     </S.OttInfoWrap>
