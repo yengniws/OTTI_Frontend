279f510e659ed1485f92e324ae74136b84cb1f69
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-09-26 09:20:54 +0900

    Fix: 파일 복구

---
+++ src/components/TopBar/DropDown.Style.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Dropdown = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  margin-left: 5px;
+`;
+
+export const DropdownSelect = styled.select`
+  border: none;
+  padding-left: 16px;
+  border: none;
+  font-size: 20px;
+  padding-top: 18px;
+  margin-top: 18px;
+  padding-bottom: 22px;
+  outline: none;
+  cursor: pointer;
+  font-weight: 400;
+  background-color: #fff;
+`;
